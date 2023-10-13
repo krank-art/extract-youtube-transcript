84d3f618692b46d5d2e05a4fc2dde3c2876e3055
Krank <kranko.art@gmail.com>
2023-10-13 15:11:57 +0200

    Add writing to output or raw

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from deepmultilingualpunctuation import PunctuationModel
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import TextFormatter
 import sys
 import re
 import requests
+import os
+from deepmultilingualpunctuation import PunctuationModel
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+
+def get_safe_filename(filename, replacement='_', max_length=255):
+    safe_filename = re.sub(r'[\\/:*?"<>|]', replacement, filename)
+    if len(safe_filename) > max_length:
+        return safe_filename[:max_length]
+    return safe_filename
 
 def get_youtube_video_title(video_id):
     response = requests.get("https://www.youtube.com/watch?v=" + video_id)
@@ -29,19 +36,48 @@ def add_punctuation(input):
 def format_sentences(input):
     capitalized_input = input[0].upper() + input[1:]
     pattern = r'([.?]) (\S)'
-    replace = lambda match: f"{match.group(1)}\n{match.group(2).upper()}"
+    replace = lambda match: f"{match.group(1)}\n\n{match.group(2).upper()}"
     return re.sub(pattern, replace, capitalized_input)
 
+def get_unique_file_path(dir, filename, extension):
+    duplicate_counter = 1
+    new_file_path = os.path.join(dir, filename + extension)
+    while (os.path.exists(new_file_path)):
+        suffix = f" {duplicate_counter}" if duplicate_counter > 1 else ""
+        new_file_path = os.path.join(dir, filename + suffix + extension)
+        duplicate_counter += 1
+    return new_file_path
+
 def main():
+    # Process CLI arguments
     if len(sys.argv) <= 1:
         print("No YouTube video id provided. Please provide one.")
         return
     video_id = sys.argv[1]
-    print(get_youtube_video_title(video_id))
-    # raw_transcript = get_transcript(video_id)
-    # punctuated_transcript = add_punctuation(raw_transcript)
-    # formatted_transcript = format_sentences(punctuated_transcript)
-    # print(formatted_transcript)
+    has_raw_output = len(sys.argv) > 2 and sys.argv[2] == "--raw"
+
+    # Get transcript
+    raw_transcript = get_transcript(video_id)
+    punctuated_transcript = add_punctuation(raw_transcript)
+    formatted_transcript = format_sentences(punctuated_transcript)
+
+    # Print to console if raw
+    if (has_raw_output):
+        print(formatted_transcript)
+        return
+
+    # Write file to output
+    output_dir = "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    video_title = get_youtube_video_title(video_id)
+    raw_title = re.search(r'(.*) - YouTube$', video_title).group(1)
+    escaped_title = get_safe_filename(raw_title)
+    file_name_base = f"({video_id}) {escaped_title}"
+    output_path = os.path.join(output_dir, file_name_base)
+    unique_output_path = get_unique_file_path(output_dir, output_path, ".txt")
+    with open(unique_output_path, 'w') as file:
+        file.write(formatted_transcript)
 
 if __name__ == "__main__":
     main()
